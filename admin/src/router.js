964b62d1f59e125d7b75bc450cb7559a202eac1d
user <79048189339@ya.ru>
2019-01-18 00:38:08 +0500

    Добавлены услуги

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 import Register from './components/Register.vue';
+import Services from './components/Services.vue';
 import store from './store';
 
 Vue.use(VueRouter);
@@ -21,6 +22,7 @@ const routes = [
         }
     },
     {path:'/login', component:Login, meta:{title:'Авторизация'}},
+    {path:'/services', component:Services, meta:{title:'Услуги'}},
     {path:'/register', component:Register, meta:{title:'Регистрация'}},
 ];
 
